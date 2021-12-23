8de614a69e05ce54584306616992cb47ff740000
Pierce <piercedavidraphael@gmail.com>
2021-12-23 10:46:40 -0600

    First push

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import Sidebar from "./Sidebar";
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"> 
+        <Sidebar />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Sidebar.js
@@ -0,0 +1,13 @@
+import Calendar from "./Calendar"
+import "./Sidebar.css"
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+        <Calendar />
+    </div>
+    )
+    
+};
+
+export default Sidebar;
