4085fdeb21da5ceaff8820d42e34db90a8579132
Christopher Valle <chrisbv@live.com>
2022-01-03 11:37:11 -0600

    Progress on event list

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import Sidebar from "./Sidebar";
 import "./App.css"
+import EventList from "./EventList";
+import { useState } from "react";
 
 function App() {
+  const [selectedDate, setSelectedDate] = useState((new Date()).toLocaleDateString("en-US"))
+
+  const dateForEventList = (p) => {
+    setSelectedDate(p)
+  }
+
   return (
     <div className="App">
       <header className="App-header"> 
-        <Sidebar />
+        <Sidebar eventDate={dateForEventList} />
+        <EventList selectedDate={selectedDate} />
       </header>
     </div>
   );
---
+++ client/src/EventList.js
@@ -0,0 +1,15 @@
+
+function EventList({selectedDate}) {
+
+
+    return(
+        <div className="event_list">
+            <h1>
+                {selectedDate}
+                {/* {eventCards} */}
+            </h1>
+        </div>
+    )
+}
+
+export default EventList;
