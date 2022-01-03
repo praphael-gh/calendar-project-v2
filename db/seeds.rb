a0a2d320d4abf7ee5bfd9c858854924e17908d10
Cristofer <cristoferlorenzana@gmail.com>
2022-01-03 12:00:43 -0600

    merge test

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
-jim = User.create(name:"Jimbo", username: "jimbotron21", password: "memes")
+    jim = User.create(name:"Jimbo", username: "jimbotron21", password: "memes")
 
 day1 = Day.create(date: "12/21/21")
+
+## pull request test
 
 event1 = Event.create(name: "Happy Day", country: "Lalaland", location: "Local", rating:3, user_id: 1, day_id: 1)
