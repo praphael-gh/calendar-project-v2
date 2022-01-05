2426912649b3912473919251ecbf4e1f5e9aef87
Christopher Valle <chrisbv@live.com>
2022-01-05 15:54:30 -0600

    Troubleshooting

--- config/application.rb
+++ config/application.rb
@@ -13,10 +13,8 @@ module CalenderProject
     config.load_defaults 7.0
 
     config.api_only = true
-
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
-
     config.action_dispatch.cookies_same_site_protection = :strict
 
     config.middleware.use Rack::Cors do
@@ -25,6 +23,5 @@ module CalenderProject
         resource '/*', :headers => :any, :methods => :patch
       end
     end
-
   end
 end
